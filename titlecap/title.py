acaf77148605b831487edd9f71ac2598333c9edd
Sarah Chen <sarahlc888@gmail.com>
2020-10-01 16:56:40 -0400

    rename main.py and titlecaps() to title.py and use_caps()

--- titlecap/title.py
+++ titlecap/title.py
@@ -1,6 +1,6 @@
 short_words = ["the", "in", "a", "as", "to", "be", "an", "for", "nor", "but", "yet", "so", "at", "around", "by", "after", "along", "for", "from", "of", "on", "to", "with", "without"]
 
-def titlecaps(s):
+def add_caps(s):
     words = s.lower().split()
     for i in range(0, len(words)):
         if words[i] not in short_words and len(words[i])>3:
